250e7bec886c9314b863a0bd3be73e6ed4938897
Prashant Mehta <prashantmehta1207@gmail.com>
2026-02-25 12:33:06 +0530

    Create average.java

---
+++ BASICS/average.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class prob1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter the three number:");
+
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+        double number3 = input.nextDouble();
+
+        double average = (number1 + number2 + number3) / 3;
+        System.out.println("the average" + number1 + " " + number2 + " " + number3 + " is " + average);
+
+    }
+}
