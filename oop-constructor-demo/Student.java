adabe974454ae40fe25a5f058b3e84f7ce6eaa27
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-12 21:50:43 +0530

    Add Student class with constructor and display method

--- oop-constructor-demo/Student.java
+++ oop-constructor-demo/Student.java
@@ -1,1 +1,27 @@
+class Student {
+    int roll;
+    String name;
+
+    // constructor
+    Student(int r, String n) {
+        roll = r;
+        name = n;
+    }
+
+    // display
+
+    void display() {
+        System.out.println(roll + " " + name);
+    }
+}
+// main class 
+public class Constructor {
+    public static void main(String[] args) {
+        Student S1 = new Student(101, "Prashant");
+        Student S2 = new Student(102, "neha");
+
+        S1.display();
+        S2.display();
+    }
+}
 
