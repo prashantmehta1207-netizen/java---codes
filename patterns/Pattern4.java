d12a382a63e0d1d06c34cbc3d52a4f4b9715eac8
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-15 23:11:29 +0530

    Add pattern printing program in Pattern4.java
    Implement a pattern printing program that outputs decreasing rows of asterisks based on user input.

---
+++ patterns/Pattern4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Patterns4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number : ");
+        int n = sc.nextInt();
+
+        // outer loop
+        for (int i = n; i >= 1; i--) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
