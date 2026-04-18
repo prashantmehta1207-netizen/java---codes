d22cf5eff1c629af8715c6e208f001cf993581e8
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-18 16:59:51 +0530

    Create Butterfly Pattern.java

---
+++ Advanced-Patterns /Butterfly Pattern.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class ADVANCEPatterns2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number  : ");
+
+        int n = sc.nextInt();
+
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            // first part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // space
+            int space = 2 * (n - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+
+            // second part
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower half
+
+        for (int i = n; i >= 1; i--) {
+            // first part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // space
+            int space = 2 * (n - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+
+            // second part
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+OUTPUT:
+
+*      *
+**    **
+***  ***
+********
+********
+***  ***
+**    **
+*      *
