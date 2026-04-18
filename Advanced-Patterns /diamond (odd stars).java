340db2e40b6ddaa512b4a7ac17f89412505c6d5b
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-18 17:30:47 +0530

    Create diamond (odd stars).java

---
+++ Advanced-Patterns /diamond (odd stars).java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ADVANCEPatterns3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number : ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+            // space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
