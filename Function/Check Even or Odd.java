b4f9fcd13a255f0a9ac3959d82e58613bba329e0
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-20 23:18:14 +0530

    Create Check Even or Odd.java

---
+++ Function/Check Even or Odd.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void checkEvenOdd(int num) {
+        if(num % 2 == 0)
+            System.out.println("Even Number");
+        else
+            System.out.println("Odd Number");
+    }
+
+    public static void main(String[] args) {
+        checkEvenOdd(7);
+    }
+}
