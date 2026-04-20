55b1dbc7e059b52178eca37adb218e6ea63c608d
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-20 23:16:36 +0530

    Create Function without Return (Void).java

---
+++ Function/Function without Return (Void).java
@@ -0,0 +1,11 @@
+public class Main {
+
+    // Function to print message
+    public static void greet() {
+        System.out.println("Hello, Welcome!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
