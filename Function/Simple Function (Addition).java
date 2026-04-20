8e68e9d5ec4134370eb2fe230c14fd2e32e8e4d7
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-20 23:15:01 +0530

    Create Simple Function (Addition).java

---
+++ Function/Simple Function (Addition).java
@@ -0,0 +1,14 @@
+public class Main {
+
+    // Function to add two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = add(5, 3);
+
+        System.out.println("Sum = " + result);
+    }
+}
