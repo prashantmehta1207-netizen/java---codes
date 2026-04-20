95d849157db3db377d70ec680114a8c7a5a783b7
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-20 23:08:22 +0530

    Create loops.java

---
+++ BASICS/loops.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // for (int counter = 0; counter < 3; counter = counter + 1) {
+        // System.out.println("hello Wrold");
+        // }
+
+        for (int n = 0; n < 2; n++) {
+            System.out.println("hello Wrold");
+        }
+    }
+}
