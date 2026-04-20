190c2b1205787e6887f3392839287884d2ccb876
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-20 23:09:32 +0530

    Create PowerExample.java

---
+++ BASICS/PowerExample.java
@@ -0,0 +1,11 @@
+// public class PowerExample {
+//     public static void main(String[] args) {
+//         System.out.println(Math.pow(2, 3));
+//     }
+// }
+
+public class PowerExample {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(3, 3));
+    }
+}
