8953bbb229781ddf532593e4c77b4d2af9359c6b
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-23 14:32:32 +0530

    Create calculationfactorial.java

---
+++ Function/calculationfactorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static int calculationfactorial(int a) {
+        int factorial = 1;
+        for (int i = 1; i <= a; i++) {
+            factorial = factorial * i;
+        }
+        return factorial;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int a = sc.nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= a; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("the factorial of " + a + " is " + factorial);
+    }
+}
