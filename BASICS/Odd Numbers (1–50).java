27ddaae7ce139cc5a3f7b4ca7897651d81746136
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-27 17:25:49 +0530

    Create Odd Numbers (1–50).java

---
+++ BASICS/Odd Numbers (1–50).java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i <= 50; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
