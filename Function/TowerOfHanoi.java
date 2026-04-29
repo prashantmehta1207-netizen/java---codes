177f120112ac976073ee21bf1e90ac563c5c4187
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-29 16:39:26 +0530

    Add files via upload

---
+++ Function/TowerOfHanoi.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+    static void toh(int n, char source, char auxiliary, char destination) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
+            return;
+        }
+
+        // Move n-1 disks from source to auxiliary
+        toh(n - 1, source, destination, auxiliary);
+
+        // Move nth disk
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+
+        // Move n-1 disks from auxiliary to destination
+        toh(n - 1, auxiliary, source, destination);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of disks: ");
+        int n = sc.nextInt();
+        toh(n, 'A', 'B', 'C');
+    }
+}
