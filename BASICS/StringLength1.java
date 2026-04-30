35ecbf26b71e4b1323e0f6deba8998f3adf1a3e2
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-30 21:12:17 +0530

    Create  StringLength1.java

---
+++ BASICS/StringLength1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class StringLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = "Java";
+        System.out.println("Length = " + str.length());
+    }
+}
