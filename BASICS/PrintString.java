0173fb1989d981950d91260954b1d359679e95fb
Prashant Mehta <prashantmehta1207@gmail.com>
2026-04-30 21:09:04 +0530

    Add files via upload

---
+++ BASICS/PrintString.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class PrintString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = "Hello Java";
+        System.out.println(name);
+    }
+}
