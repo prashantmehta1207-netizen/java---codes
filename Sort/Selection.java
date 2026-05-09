7d5233287b3b61a68d32425ccb9e4d43f08172c3
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-09 20:39:12 +0530

    Create Selection.java

---
+++ Sort/Selection.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input size of array
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        // Input array elements
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Selection Sort
+        for (int i = 0; i < n - 1; i++) {
+
+            int minIndex = i;
+
+            // Find minimum element
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap elements
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        // Print sorted array
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
