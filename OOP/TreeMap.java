a88c108367d5fa02f0f5d085cf8e1bcfb7431065
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-13 19:42:26 +0530

    Create TreeMap.java

---
+++ OOP/TreeMap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Demo {
+    public static void main(String args[]) {
+        TreeMap<Integer, String> tm = new TreeMap<>();
+
+        tm.put(3, "Mango");
+        tm.put(1, "Apple");
+        tm.put(2, "Banana");
+
+        System.out.println(tm);
+    }
+}
