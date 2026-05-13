223137ebba2f7f4eea4410f7b0d1fc2f91204308
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-13 19:41:31 +0530

    Create HashMap.java

---
+++ OOP/HashMap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Demo {
+    public static void main(String args[]) {
+        HashMap<Integer, String> hm = new HashMap<>();
+
+        hm.put(1, "Apple");
+        hm.put(2, "Banana");
+        hm.put(3, "Mango");
+
+        System.out.println(hm);
+    }
+}
