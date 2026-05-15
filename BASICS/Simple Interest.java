0d36ab603e3943005be6ca5ab2e44a821e5c35bf
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-15 20:42:08 +0530

    Create Simple Interest.java

---
+++ BASICS/Simple Interest.java
@@ -0,0 +1,11 @@
+class Interest {
+    public static void main(String args[]) {
+        int p = 1000;
+        int r = 5;
+        int t = 2;
+
+        int si = (p * r * t) / 100;
+
+        System.out.println("Simple Interest = " + si);
+    }
+}
