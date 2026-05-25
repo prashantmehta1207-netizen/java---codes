aed106e7e4e242db0e420f6678e2ef05bc2afbd8
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-25 17:57:09 +0530

    Create Create LinkedList.java

---
+++ OOP/LinkedList/Create LinkedList.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Test {
+    public static void main(String[] args) {
+        LinkedList<String> names = new LinkedList<>();
+
+        names.add("mahi");
+        names.add("prashant");
+        names.add("mehta");
+
+        System.out.println(names);
+
+    }
+}
