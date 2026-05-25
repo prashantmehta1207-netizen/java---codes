068c205e21059eaa31a13086930c5128520bb76e
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-25 18:10:02 +0530

    Create Size.java

---
+++ OOP/LinkedList/Size.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class SizeExample {
+    public static void main(String[] args) {
+
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("A");
+        list.add("B");
+
+        System.out.println(list.size());
+    }
+}
