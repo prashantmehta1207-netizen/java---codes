ce43608532cb0eb94b7d194d1b9eccaa47718714
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-26 21:09:04 +0530

    Create Get.java

---
+++ OOP/ArrayList/Get.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class GetExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Red");
+        list.add("Blue");
+
+        System.out.println(list.get(1));
+    }
+}
