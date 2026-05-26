fdb396d8ea3ff74d3e31b039749c1d105365217a
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-26 21:08:29 +0530

    Create AddExample.java

---
+++ OOP/ArrayList/AddExample.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class AddExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Python");
+
+        System.out.println(list);
+    }
+}
