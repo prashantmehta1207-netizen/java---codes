1936d53e8fce7d812524159c8eb275711588b94f
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-26 21:10:22 +0530

    Add SizeExample class to demonstrate ArrayList size

---
+++ OOP/ArrayList/Size.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class SizeExample {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        System.out.println(numbers.size());
+    }
+}
