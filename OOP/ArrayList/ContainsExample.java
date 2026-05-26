ab303ac99f03a0e36768fdef379b03e7b6662518
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-26 21:10:59 +0530

    Create ContainsExample.java

---
+++ OOP/ArrayList/ContainsExample.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ContainsExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+
+        System.out.println(list.contains("Java"));
+    }
+}
