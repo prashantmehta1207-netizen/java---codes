e62c821328cf3edc28e7b7ac70476911a86e9aec
Prashant Mehta <prashantmehta1207@gmail.com>
2026-06-30 22:56:28 +0530

    Create Maximum of Two Numbers.java

---
+++ Function/Maximum of Two Numbers.java
@@ -0,0 +1,12 @@
+public class Demo {
+    static int max(int a, int b) {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(25, 18));
+    }
+}
